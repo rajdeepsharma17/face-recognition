f5c1e7076331b9bd6bce2da47ddae6aa64fe895e
Rajdeep Sharma <rajdeep.sharma17@gmail.com>
2018-02-06 22:42:37 +0530

    added particle background

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import './App.css';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
+        
       <div className="App">
-       <Navigation />
-       <Logo />
-       <ImageLinkForm />
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+       
         {/*
        <FaceRecongnition />*/}
       </div>
